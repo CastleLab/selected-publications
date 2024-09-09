09a8b3e5b5034b63614ebcfdd3b3e09786c6cf78
CongyingXU <1084729816@qq.com>
2024-09-09 17:47:16 +0800

    udpate congying's pub

--- collection/Congying_Xu.js
+++ collection/Congying_Xu.js
@@ -7,38 +7,23 @@ module.exports = [
         ],
         venue: "ACM Transactions on Software Engineering and Methodology",
         venueShort: "TOSEM",
-        // tags: [
-        //     "Tags are those keywords used to categorize the publication by:",
-        //     "Research topic",
-        //     "Methodology",
-        //     "Please consider reuse the existing tags in the publication website: http://sccpu2.cse.ust.hk/castle/publication/",
-        //     "Users can use tags to filter and find the publications they are interested in"
-        // ],
-        // awards: [
-        //     "Any awards received by this paper:",
-        //     "Distinguished Paper"
-        // ],
-        // abstract: `
-        //     (optional) abstract of your paper (support multiline)
-        // `,
+        tags: [
+            "Meramorphic Testing"
+        ],
         projectUrl: "https://mr-scout.github.io/",
         paperUrl: "https://dl.acm.org/doi/abs/10.1145/3656340",
-        // arxivUrl: "(optional) the arxiv version of your paper",
-        // slidesUrl: "(optional) slides url. You can put the file in assets folder and refer to it here with {ASSETS}/your-slides.pdf",
-        // bibtex: ` (optional)
-        //     @article{paper
-        //         ......
-        //     }
-        // `
     },
     {
         title: "MR-Scout: Automated Synthesis of Metamorphic Relations from Existing Test Cases",
         date: "2024-09-01",
         authors: [
-            "Congying Xu", "Valerio Terragni", "Hengcheng Zhu", "Jiarong Wu", "Shing-Chi Cheung"
+            "Congying Xu", "Songqiang Chen", "Jiarong Wu", "Shing-Chi Cheung", "Valerio Terragni", "Hengcheng Zhu", "Jialun Cao"
         ],
         venue: "IEEE/ACM International Conference on Automated Software Engineering",
         venueShort: "ASE",
+        tags: [
+            "LLM", "Meramorphic Testing"
+        ],
         projectUrl: "https://mr-adopt.github.io/",
         paperUrl: "https://arxiv.org/abs/2408.15815",
     }
